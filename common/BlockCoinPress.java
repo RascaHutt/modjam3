d2b01b6ee23b9fc7d7f44f8a675b1f0888fd8ff8
scorpianz1525 <jacquesrascagneres@gmail.com>
2013-12-14 18:55:39 +0000

    Coin Press Texture

--- common/BlockCoinPress.java
+++ common/BlockCoinPress.java
@@ -1,11 +1,20 @@
 package assets.modjam3.common;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 public class BlockCoinPress extends BlockContainer{
@@ -16,7 +25,36 @@ public class BlockCoinPress extends BlockContainer{
 		setCreativeTab(CreativeTabs.tabBlock);
 		setHardness(3);
 	}
-
+	
+	@SideOnly(Side.CLIENT)
+	private Icon side, bottom, top, front;
+	
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister reg){
+		this.side = reg.registerIcon(Reference.MOD_TEXTUREPATH + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1) + "_side");
+		this.bottom = reg.registerIcon(Reference.MOD_TEXTUREPATH + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1) + "_top");
+		this.top = reg.registerIcon(Reference.MOD_TEXTUREPATH + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1) + "_top");
+		this.front = reg.registerIcon(Reference.MOD_TEXTUREPATH + ":" + this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1) + "_front");
+	}
+	
+	@SideOnly(Side.CLIENT)
+    @Override
+    public Icon getIcon(int side, int metadata){
+		if (side == 1) return this.top;
+		else if (side == 0) return this.top;
+		else if (metadata == 2 && side == 2) return this.front;
+	      else if (metadata == 3 && side == 5) return this.front;
+	      else if (metadata == 0 && side == 3) return this.front;
+	      else if (metadata == 1 && side == 4) return this.front;
+	      else return this.side;
+	}
+	
+	public void onBlockPlacedBy(World par1World, int x, int y, int z, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack)
+    {
+        int whichDirectionFacing = MathHelper.floor_double((double)(par5EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3;
+        par1World.setBlockMetadataWithNotify(x, y, z, whichDirectionFacing, 2);
+    }
+	
 	@Override
 	public TileEntity createNewTileEntity(World world) {
 		// TODO Auto-generated method stub
@@ -33,4 +71,5 @@ public class BlockCoinPress extends BlockContainer{
  }
          return false;
     }
+    
 }
