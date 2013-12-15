85236c7b19b40c01a3686d6120a34deba965bf77
scorpianz1525 <jacquesrascagneres@gmail.com>
2013-12-15 13:01:14 +0000

    Banker Texture

--- common/DefaultProps.java
+++ common/DefaultProps.java
@@ -5,10 +5,7 @@ import net.minecraft.util.ResourceLocation;
 public class DefaultProps {
 	  public  static final ResourceLocation COIN_PRESS_GUI =new ResourceLocation("modjam3:textures/gui/coinpress.png");
 	  public  static final ResourceLocation STOCK_VIEWER_GUI =new ResourceLocation("modjam3:textures/gui/stockviewer.png");
-
 	  public  static final ResourceLocation BANKER_SKIN =new ResourceLocation("modjam3:textures/entities/banker.png");
-
-
 	  public  static final ResourceLocation STOCK_TRADER_GUI =new ResourceLocation("modjam3:textures/gui/stocktrader.png");
 
 }
