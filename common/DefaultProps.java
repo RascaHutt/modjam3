53fddbd8c8f1c4de743c4510a1e489d1285aeb6a
mhutti1 <mhutti1@gmail.com>
2013-12-15 12:35:40 +0000

    fix

--- common/DefaultProps.java
+++ common/DefaultProps.java
@@ -5,10 +5,10 @@ import net.minecraft.util.ResourceLocation;
 public class DefaultProps {
 	  public  static final ResourceLocation COIN_PRESS_GUI =new ResourceLocation("modjam3:textures/gui/coinpress.png");
 	  public  static final ResourceLocation STOCK_VIEWER_GUI =new ResourceLocation("modjam3:textures/gui/stockviewer.png");
-<<<<<<< HEAD
+
 	  public  static final ResourceLocation BANKER_SKIN =new ResourceLocation("modjam3:textures/entities/banker.png");
 
-=======
+
 	  public  static final ResourceLocation STOCK_TRADER_GUI =new ResourceLocation("modjam3:textures/gui/stocktrader.png");
->>>>>>> f86c1485ead23ee2c9ec9f3fc0fcb2fcd9928a66
+
 }
